86c9ac5e0905276105ea108c80dc1160075f4ed6
Prabesh <weeareone321@gmail.com>
2025-03-03 23:00:53 +0545

    My day 2 of learning python completed

--- Pythonall/Pythond2.py
+++ Pythonall/Pythond2.py
@@ -8,8 +8,66 @@
 
 # Exercise to calsulate area of rectangle
 
-Area = int(input("Enter the Length:"))
-Area2 = int(input("Enter the breadth:"))
-Area3 = Area * Area2
-print(f"Area of rectangle is {Area3}")
+# Area = int(input("Enter the Length:"))
+# Area2 = int(input("Enter the breadth:"))
+# Area3 = Area * Area2
+# print(f"Area of rectangle is {Area3}")
+
+
+# Exercise 2 shopping cart Program
+
+# item = input("what item do you want ot buy?:")
+# price = float(input("What is the price?: "))
+# quantity = int(input("how many would you like to buy:"))
+# total = price * quantity
+# print(f"you have to pay {total} for {item}")
+
+# Exercise 3 Madlibs game where you create a story by filling in blanks with random words
+# Guess about her and why do you like her?
+# description = input("Enter how she looks like:")
+# description2 = input("Enter her personality:")
+# description3 = input("Enter her another best personality:")
+# description4 = input("Enter what she takes of you:")
+# description5 = input("Enter what she always do till death:")
+# print("I love her because:")
+# print(f"she is {description}.")
+# print(f"she is {description2} and {description3} ")
+# print(f"The world doesnot {description4} about me but she will always {description4} about meand {description5} me")
+
+# Builtin math functions
+# round() 3.223 to 3
+# abs() -4 to 4
+# pow(2,3) 2 to the power of 3 = 8  (2*2*2)
+# max() 2,3,4 = 4
+# min() 2,3,4 = 2
+
+
+# import math
+
+# x = 4.1
+# print(math.pi)
+# print(math.e)
+# result = math.floor(x) it will round down the number
+# result = math.ceil(x) # it will round up the number and return the integer
+# result = math.sqrt(x) # it will return the square root of the number
+# print(result)
+
+
+# Exercise to calculate Circumference of circle using math functions
+
+# import math
+# r = int(input("Enter the radius:"))
+# pie = math.pi
+# circumference = 2 * pie * r
+# print(f"The circumference of the circle is {round(circumference,3)}")
+
+
+# Exercise to calculate area of circle using math functions
+
+# import math
+# r = float(input("Enter the radius:"))
+# area_of_circle = math.pi * (r**2)
+# print(f"The area of the circle is {round(area_of_circle,3)}")  # round
+
+
 
