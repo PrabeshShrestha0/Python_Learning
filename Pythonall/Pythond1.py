6665d74eb033407f609af2b2299e284de29b3dfd
Prabesh <weeareone321@gmail.com>
2025-03-03 14:08:19 +0545

    My Python learning

---
+++ Pythonall/Pythond1.py
@@ -0,0 +1,68 @@
+#c1 To print
+
+# print("i love me")
+# print("i love all")
+
+
+# 2
+#  Varaiable it behaves as it contains a value
+#  A container for a value
+
+# 2.i Sring are series of text i. e pra = "prabesh"
+# first_name = "Prabesh"
+# last_name = "Kumar"
+# food = "Pizza"
+# email = "qwe@gmail.com"
+# print(f"hello {first_name}")
+# print(f"YOUR FAVOURITE FOOD IS {food}")
+# print(f"YOUR EMAIL IS {food}")
+
+
+# 3 Integers is a whole number 
+# age = 22
+# print(f"your age is {age}")
+
+
+# 4 float is a datatype that can store decimal values
+
+# floatt = 23.45
+# print(f"this is float with decimal value: {floatt}")
+
+
+# 5 Boolean is a datatype that stores 2 values either it is true or false
+ 
+# 5.i
+# for_sale = True
+
+# if for_sale:
+#     print("you sell")
+# else:
+#     print("you didnot sell")
+
+
+# 5.ii
+# for_sale = False
+
+# if for_sale:
+#     print("you sell")
+# else:
+#     print("you didnot sell")
+
+
+# 6 Type casting is a process of converting a varaiblr one data type to another data type
+Series_of_text = "Prabesh"
+decimal_value = 28.32
+whole_number = 23
+Truee_Falsee = True
+
+# print(type(Series_of_text))
+# print(type(decimal_value))
+# print(type(whole_number))
+# print(type(Truee_Falsee))
+
+# whole_number = int(decimal_value)
+# print(whole_number)
+#converting decimal to whole number
+
+Series_of_text = str(whole_number)
+print(Series_of_text)
