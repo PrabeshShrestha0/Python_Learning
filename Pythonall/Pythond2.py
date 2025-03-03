bc214a38d4b082626c2c6bf0c7a3697be0ef36d3
Prabesh <weeareone321@gmail.com>
2025-03-03 14:34:02 +0545

    This is my python learning day 2 about the input

--- Pythonall/Pythond2.py
+++ Pythonall/Pythond2.py
@@ -1,9 +1,15 @@
-# write a function and calculate 3 number average
+#  input() it ask a prompt from a user to enter the darta and return it into string
 
-def claculate_average(a , b, c):  
-    sum = a + b + c
-    average = sum / 3
-    print(average)
-    return average
+# Prabesh = str(input("Enter your name:")) 
+# Age = int(input("Enter your Age:")) 
+# print(f"i got it your name is {Prabesh} and Many Happy Returns of the day {Prabesh}")
+# print(f"Now you are {Age} years old and i am happy to know that") # it will throw an error because we are trying to print an integer
 
-claculate_average(10, 20, 30)   
+
+# Exercise to calsulate area of rectangle
+
+Area = int(input("Enter the Length:"))
+Area2 = int(input("Enter the breadth:"))
+Area3 = Area * Area2
+print(f"Area of rectangle is {Area3}")
+
