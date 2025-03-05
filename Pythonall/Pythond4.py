f63d5ee1af57e13d0d09dadd78918e05bd42bf51
Prabesh <weeareone321@gmail.com>
2025-03-05 23:33:17 +0545

    i did some exercise and learn a short about logical operator

---
+++ Pythonall/Pythond4.py
@@ -0,0 +1,25 @@
+# Temperature conversion exercise
+
+# unit = input("Enter temperature in celcius or fahrenheit (C/F):")
+# temp = float(input("Enter temperature:"))
+
+# if unit == "C":
+#     temp = round((temp * 9/5) + 32)
+#     print(f"The temperature in fahrenheit is {temp}°F")
+# elif unit == "F":
+#     temp = round((temp - 32) * 5/9)
+#     print(f"The temperature in celcius is {temp}°C")
+# else:
+#     print(f"(unit) is Invalid unit. Please enter C or F.")
+
+
+# logical operators evaluate multiple conditions
+# and operator  = both conditions must be true
+# or opetrator = at least one condition must be true
+# not operator = the condition is false
+
+# Program with all use of and or and not operator
+
+
+
+
