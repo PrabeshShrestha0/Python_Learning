5396b5919f083762668a2265d3ad09f49af4f619
Prabesh <weeareone321@gmail.com>
2025-03-09 22:24:08 +0545

    i learned abot for loops and make a timer from it

---
+++ Pythonall/Pythond8.py
@@ -0,0 +1,47 @@
+# For Loop = it executes block of code for a fixed num of times
+
+
+# crdit_card = "1234-2345-4566-3322"
+
+# for x in crdit_card:
+#     print(x)
+
+
+# for x in range(1,45,4):
+#     if x == 37:
+#         break
+#     else:
+#         print(x)
+
+
+# Exercise count down timer
+
+# import time
+
+# my_time = int(input("Enter the time in sec:"))
+
+# for x in range(0, my_time):
+#     print(x)
+#     time.sleep(2)
+
+# print("times is up")
+
+# above code 0 dekhi suru vayera 7 samma janxa 2 secs ko farak ma and times up
+
+
+import time
+
+# Get user input
+my_time = int(input("Enter the time in seconds: "))
+
+# Countdown loop
+for remaining_time in range(my_time, 0, -1):
+    hours = remaining_time // 3600
+    minutes = (remaining_time % 3600) // 60
+    seconds = remaining_time % 60
+
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time is up!")
+
