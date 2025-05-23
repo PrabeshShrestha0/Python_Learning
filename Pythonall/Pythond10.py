da391cee81239153d9b8075481e0595fbbf4b3b2
Prabesh <weeareone321@gmail.com>
2025-05-23 17:01:39 +0545

    i learned about calling my name is Prabesh Shrestha

---
+++ Pythonall/Pythond10.py
@@ -0,0 +1,11 @@
+ # Shoppin cart Program
+
+ food = []
+ prices = []
+ total = 0
+
+
+ while True:
+    food =input("Enter a food to buy(q to quit):")
+    if food.lower() == "q":
+    break
---
+++ Pythond11.py
@@ -0,0 +1,2 @@
+var_name = "Prabesh"
+print(f"Hello, {var_name}!")
